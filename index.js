218035e0b4b3f1bc9a33eae20f0baaa741e00a27
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-05-27 10:49:20 +0530

    now working

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import pg from "pg"
 const app = express();
 const port = 3000;
 const db = new pg.Client({
-  username: "postgres",
+  user: "postgres",
   host: "localhost",
   database: "world",
   password: process.env.PASSWORD,
